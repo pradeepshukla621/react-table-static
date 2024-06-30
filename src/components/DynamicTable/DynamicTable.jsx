ca16d87471012c617c5946e15fb7974d7878299b
Pradeep Shukla <pradeepshukla621@gmail.com>
2024-06-30 12:30:14 +0530

    first commit

---
+++ src/components/DynamicTable/DynamicTable.jsx
@@ -0,0 +1,300 @@
+import "./DynamicTable.css"
+import eyeIcon from "../../assets/images/eye-solid.svg"
+
+export default function DynamicTable() {
+    return (
+        <div className="wrap-table">
+            <div className="wrap-content">
+                <h1 className="table-heading">User Data</h1>
+                <table className="table">
+                    <tr>
+                        <th>S.No.</th>
+                        <th>Name </th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Website</th>
+                        <th>Action</th>
+                    </tr>
+
+                    <tr>
+                        <td>1</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>3</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>4</td>
+                        <td>Patricia Lebsack	</td>
+                        <td>Karianne</td>
+                        <td>Julianne.OConner@kory.org		</td>
+                        <td>493-170-9623 x156		</td>
+                        <td>kale.biz		</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>5</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>6</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>7</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>8</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>9</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>Leanne Graham	</td>
+                        <td>Bret</td>
+                        <td>Sincere@april.biz	</td>
+                        <td>1-770-736-8031 x56442	</td>
+                        <td>hildegard.org	</td>
+                        <td><img src={eyeIcon} alt="" /></td>
+                    </tr>
+
+
+
+
+                </table>
+            </div>
+
+
+        </div>
+    )
+}
